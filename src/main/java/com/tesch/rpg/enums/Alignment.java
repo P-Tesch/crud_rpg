b2188fce6ce7bf6d8be407d38207915aa08fa011
Tesch <pedrohenriquetesch@gmail.com>
2024-02-08 18:30:20 -0300

    Simpler enums

--- src/main/java/com/tesch/rpg/enums/Alignment.java
+++ src/main/java/com/tesch/rpg/enums/Alignment.java
@@ -1,20 +1,16 @@
 package com.tesch.rpg.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public enum Alignment {
-    
-    FIRE("fire"),
-    WATER("water"),
-    AIR("air"),
-    EARTH("earth"),
-    ELECTRICITY("electricity"),
-    ARCANA("arcana"),
-    ICE("ice"),
-    VOID("void");
 
-    private String value;
+    FIRE,
+    WATER,
+    AIR,
+    EARTH,
+    ELECTRICITY,
+    ARCANA,
+    ICE,
+    VOID;
 }
--- src/main/java/com/tesch/rpg/enums/Organization.java
+++ src/main/java/com/tesch/rpg/enums/Organization.java
@@ -1,16 +1,12 @@
 package com.tesch.rpg.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public enum Organization {
     
-    EXECUTORS("executors"),
-    KNIGHTS("knights"),
-    BROTHERHOOD("brotherhood"),
-    EXORCISTS("exorcists");
-
-    private String value;
+    EXECUTORS,
+    KNIGHTS,
+    BROTHERHOOD,
+    EXORCISTS;
 }
